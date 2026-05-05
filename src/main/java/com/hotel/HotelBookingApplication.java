d6644fda91aafeb09b6dafa5937a6ec502968f65
theodora <the.doreto@gmail.com>
2026-05-05 23:27:40 +0300

    fix simple spring override

--- src/main/java/com/hotel/HotelBookingApplication.java
+++ src/main/java/com/hotel/HotelBookingApplication.java
@@ -1,6 +1,10 @@
 package com.hotel;
 import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HotelBookingApplication {
+
     public static void main(String[]  args) {
         SpringApplication.run(HotelBookingApplication.class, args);
     }
