dcc938df5f2a7bd427c011f42ee0a02a0c8845c1
theodora <the.doreto@gmail.com>
2026-05-05 23:08:33 +0300

    add simple spring

---
+++ src/main/java/com/hotel/HotelBookingApplication.java
@@ -0,0 +1,7 @@
+package com.hotel;
+import org.springframework.boot.*;
+public class HotelBookingApplication {
+    public static void main(String[]  args) {
+        SpringApplication.run(HotelBookingApplication.class, args);
+    }
+}
