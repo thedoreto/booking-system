6974e8322d88a05332012a878c3434b60c2cbcf2
thedoreto <the.doreto@gmail.com>
2026-05-08 12:12:33 +0300

    changes in Room entity

--- src/main/java/com/hotel/model/Room.java
+++ src/main/java/com/hotel/model/Room.java
@@ -1,30 +1,60 @@
 package com.hotel.model;
 
-import com.hotel.model.enums.BookingStatus;
 import com.hotel.model.enums.RoomType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "rooms")
 public class Room {
-    private static int idCount = 0;
-    @Id private final int id;
-    private final int roomNumber;
-    private RoomType type;//SINGLE, DOUBLE, APARTMENT
+
+    @Id
+    private String id;
+
+    private int roomNumber;
+
+    private RoomType type;  // SINGLE, DOUBLE, APARTMENT
+
     private double pricePerNight;
 
-
-    public Room(RoomType type, double pricePerNight, int roomNumber) {
-        this.id = idCount++;
-        this.type = type;
-        this.pricePerNight = pricePerNight;
-          this.roomNumber = roomNumber;
+    public Room() {
     }
 
-    public int getId() { return id; }
-    public int getRoomNumber() { return roomNumber; }
-    public RoomType getType() { return type; }
-    public double getPricePerNight() { return pricePerNight; }
+    public Room(String id, int roomNumber, RoomType type, double pricePerNight) {
+        this.id = id;
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.pricePerNight = pricePerNight;
+    }
 
+    public String getId() {
+        return id;
+    }
 
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
 }
