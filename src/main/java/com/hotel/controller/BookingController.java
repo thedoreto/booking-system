f9e6292396851323536c56709f34a68c1d685ebd
thedoreto <the.doreto@gmail.com>
2026-05-09 14:48:26 +0300

    allowing access from React project URL

--- src/main/java/com/hotel/controller/BookingController.java
+++ src/main/java/com/hotel/controller/BookingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 public class BookingController {
 
