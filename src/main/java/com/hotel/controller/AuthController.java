02b6547bdf3fdba335aa5ffc15426524473294bc
thedoreto <the.doreto@gmail.com>
2026-05-14 10:04:08 +0300

    add login security

---
+++ src/main/java/com/hotel/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.hotel.controller;
+
+import com.hotel.repository.UserRepository;
+import com.hotel.security.JwtUtil;
+import com.hotel.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+
+        User dbUser = userRepo.findByUsername(user.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!dbUser.getPassword().equals(user.getPassword())) {
+            return ResponseEntity.status(401).body("Wrong password");
+        }
+
+        String token = jwtUtil.generateToken(dbUser.getUsername());
+
+        return ResponseEntity.ok(Map.of("token", token));
+    }
+}
